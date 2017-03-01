4a72405247117aacfb2e816f1ac2a174ffbace0d
Rob Dickinson <robfromboulder@gmail.com>
2017-03-01 01:07:42 -0700

    Add unit test scaffolding

---
+++ test/index.js
@@ -0,0 +1,12 @@
+// © 2016-2017 Resurface Labs LLC
+
+var assert = require('chai').assert;
+var index = require('../index');
+
+describe('Index', function () {
+    describe('#default', function () {
+        it('should do something by default', function () {
+            assert.equal(1, 1);
+        });
+    });
+});
