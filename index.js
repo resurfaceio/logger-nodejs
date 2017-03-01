b6cf8b864dc262d64658ec72f6bf94e2030f9900
Rob Dickinson <robfromboulder@gmail.com>
2017-03-01 01:25:42 -0700

    Minor tweaks

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // © 2016-2017 Resurface Labs LLC
 
 module.exports = function () {
-    var version = require('./package.json').version;
+    const version = require('./package.json').version;
     console.log(`Using resurfaceio-logger-nodejs, version ${version}`);
 };
--- test/index.js
+++ test/index.js
@@ -1,11 +1,12 @@
 // © 2016-2017 Resurface Labs LLC
 
-var assert = require('chai').assert;
-var index = require('../index');
+const assert = require('chai').assert;
+let index = require('../index');
 
 describe('Index', function () {
     describe('#default', function () {
-        it('should do something by default', function () {
+        it('calls default method', function () {
+            index();
             assert.equal(1, 1);
         });
     });
