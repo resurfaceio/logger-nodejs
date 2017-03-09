44821cdcff3607b6e81033893b45b850c75bc0a7
Rob Dickinson <robfromboulder@gmail.com>
2017-03-08 22:04:13 -0700

    Change BaseLogger from abstract to concrete

--- test/base_logger.js
+++ test/base_logger.js
@@ -1,20 +1,46 @@
 // © 2016-2017 Resurface Labs LLC
 
-const BaseLogger = require('../lib/base_logger');
+const BaseLogger = require('../lib/all').BaseLogger;
 
 const chai = require('chai');
 const expect = chai.expect;
 
+/**
+ * Tests against basic usage logger to embed or extend.
+ */
 describe('BaseLogger', function () {
 
-    describe('#constructor', function () {
-        it('prevents construction', function () {
-            try {
-                expect(new BaseLogger()).to.not.exist;
-            } catch (TypeError) {
-                // this is expected
-            }
-        });
+    it('creates a valid frozen object', function () {
+        const logger = new BaseLogger();
+        expect(logger).to.exist;
+        expect(logger.constructor['name']).to.equal('BaseLogger');
+        expect(Object.isFrozen(logger)).to.be.true;
+    });
+
+    it('provides valid version', function () {
+        const version = BaseLogger.version_lookup();
+        expect(version).to.exist;
+        expect(version).to.be.a('string');
+        expect(version.length).to.be.above(0);
+        expect(version).to.startsWith('1.0.');
+        expect(version.indexOf('\\')).to.be.below(0);
+        expect(version.indexOf('\"')).to.be.below(0);
+        expect(version.indexOf('\'')).to.be.below(0);
+        const logger = new BaseLogger();
+        expect(logger.version).to.equal(BaseLogger.version_lookup());
+        expect(logger._version).to.equal(BaseLogger.version_lookup());
+        expect(logger['version']).to.equal(BaseLogger.version_lookup());
+        expect(logger['_version']).to.equal(BaseLogger.version_lookup());
+    });
+
+    it('skips writes to version', function () {
+        const logger = new BaseLogger();
+        logger._version = 'X.Y.Z';
+        logger['_version'] = 'X.Y.Z';
+        expect(logger.version).to.equal(BaseLogger.version_lookup());
+        logger.version = 'X.Y.Z';
+        logger['version'] = 'X.Y.Z';
+        expect(logger.version).to.equal(BaseLogger.version_lookup());
     });
 
 });
--- test/http_logger.js
+++ test/http_logger.js
@@ -6,28 +6,16 @@ const chai = require('chai');
 const expect = chai.expect;
 chai.use(require('chai-string'));
 
+/**
+ * Tests against usage logger for HTTP/HTTPS protocol.
+ */
 describe('HttpLogger', function () {
 
-    describe('#constructor', function () {
-        it('constructs instance and confirms version', function () {
-            const logger = new HttpLogger();
-            expect(logger).to.exist;
-            expect(logger.constructor.name).to.equal('HttpLogger');
-            expect(logger.version).to.equal(HttpLogger.version_lookup());
-        });
-    });
-
-    describe('#version_lookup', function () {
-        it('returns valid value', function () {
-            const version = HttpLogger.version_lookup();
-            expect(version).to.exist;
-            expect(version).to.be.a('string');
-            expect(version.length).to.be.above(0);
-            expect(version).to.startsWith('1.0.');
-            expect(version.indexOf('\\')).to.be.below(0);
-            expect(version.indexOf('\"')).to.be.below(0);
-            expect(version.indexOf('\'')).to.be.below(0);
-        });
+    it('creates a valid frozen object', function () {
+        const logger = new HttpLogger();
+        expect(logger).to.exist;
+        expect(logger.constructor['name']).to.equal('HttpLogger');
+        expect(Object.isFrozen(logger)).to.be.true;
     });
 
 });
