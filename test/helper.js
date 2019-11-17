13aa1bb49432017e8ec4e650221efd33285954d8
Rob Dickinson <robfromboulder@gmail.com>
2019-11-17 13:07:06 -0700

    #22 Fix for random timeouts under test

--- test/helper.js
+++ test/helper.js
@@ -1,7 +1,7 @@
 // © 2016-2019 Resurface Labs Inc.
 
 const resurfaceio = require('../lib/all');
-const DemoURL = 'https://demo-resurfaceio.herokuapp.com/ping';
+const DemoURL = 'https://demo.resurface.io/ping';
 const HttpRequestImpl = resurfaceio.HttpRequestImpl;
 const HttpResponseImpl = resurfaceio.HttpResponseImpl;
 
