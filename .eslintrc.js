64cd17c7c6168880a1027833cd1fc5740a28d85a
Anyesh <sthacruz@gmail.com>
2021-04-14 10:16:06 +0545

    new: Update eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,7 @@ module.exports = {
     browser: true,
     commonjs: true,
     es2021: true,
+    mocha: true
   },
   extends: [
     'airbnb-base', 'prettier'
@@ -10,10 +11,12 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 12,
   },
+  ignorePatterns: ["*_spec.js"],
   rules: {
           
       "no-underscore-dangle": 'off',
-      "global-require": 0  
+      "global-require": 0 ,
+      'camelcase': 'off', 
   
   },
 };
